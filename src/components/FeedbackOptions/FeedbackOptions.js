f64e6987815884b761d64aedd56b32c61d1e65f9
Saharok0707 <saharok0707@gmail.com>
2022-04-23 14:46:24 +0300

    q

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./FeedbackOptions.css";
+
+
+const FeedbackOptions=({options,onLeaveFeedback})=>(
+  <div>
+    <ul className="optionList">
+      {options.map((option)=>{
+        return(
+          <li key={option} className="optionItem">
+            <button
+            className="optionBtn"
+            type="button"
+            onClick={()=>{
+            onLeaveFeedback(option);
+            }}
+        >
+          {option}
+          </button>
+  </li>
+  );
+          })}
+</ul>
+</div>
+);
+
+FeedbackOptions.propTypes={
+  option:PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback:PropTypes.func.isRequired,
+};
+export default FeedbackOptions;
